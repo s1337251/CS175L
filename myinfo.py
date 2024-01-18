c2204157fd0db13dce41004f476c2a5133b894ba
s1337251 <146123133+s1337251@users.noreply.github.com>
2024-01-18 12:57:31 -0500

    Add files via upload
    my information v1

---
+++ myinfo.py
@@ -0,0 +1,12 @@
+Python 3.9.2 (default, Feb 28 2021, 17:03:44) 
+[GCC 10.2.1 20210110] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #CS175L
+>>> #Jimmy Kong
+>>> #myInfo
+>>> print('Jimmy Kong')
+Jimmy Kong
+>>> print('123 Any Street, Bridgewater, NJ 08807')
+123 Any Street, Bridgewater, NJ 08807
+>>> print('908-547-7302')
+908-547-7302
