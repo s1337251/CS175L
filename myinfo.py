7bd948a6e62fff1a1a8541c874b813eb17b4792e
s1337251 <146123133+s1337251@users.noreply.github.com>
2024-01-22 12:05:32 -0500

    my information v1

---
+++ myinfo.py
@@ -0,0 +1,9 @@
+>>> #CS175L
+>>> #Jimmy Kong
+>>> #myInfo
+>>> print('Jimmy Kong')
+Jimmy Kong
+>>> print('123 Any Street, Bridgewater, NJ 08807')
+123 Any Street, Bridgewater, NJ 08807
+>>> print('908-547-7302')
+908-547-7302
