e359b8e83607c669adbb6fcf185cc6702cf1c2e1
s1337251 <146123133+s1337251@users.noreply.github.com>
2024-01-22 11:57:07 -0500

    Update myinfo.py

--- myinfo.py
+++ myinfo.py
@@ -1,6 +1,3 @@
-Python 3.9.2 (default, Feb 28 2021, 17:03:44) 
-[GCC 10.2.1 20210110] on linux
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> #CS175L
 >>> #Jimmy Kong
 >>> #myInfo
